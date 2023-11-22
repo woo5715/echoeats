2eee0c90244463ddb7fb9d410c383cbf4bf20a50
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-22 12:29:07 +0900

    [refactor] ProdDao -> ProdRepository

--- src/main/java/com/pofol/main/product/repository/ProdRepository.java
+++ src/main/java/com/pofol/main/product/repository/ProdRepository.java
@@ -2,7 +2,7 @@ package com.pofol.main.product.repository;
 
 import com.pofol.main.product.domain.ProdDto;
 
-public interface ProdDao {
+public interface ProdRepository {
 
     ProdDto select(Integer pro_id) throws Exception;
 
--- src/main/java/com/pofol/main/product/repository/ProdRepositoryImpl.java
+++ src/main/java/com/pofol/main/product/repository/ProdRepositoryImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProdDaoImpl implements ProdDao {
+public class ProdRepositoryImpl implements ProdRepository {
 
     @Autowired
     private SqlSession session;
