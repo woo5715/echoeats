1c93ca6dbac0db7c1cbaf42ec769a0d2b05033fa
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-24 15:46:40 +0900

    [feat]상품 이미지 업로드(썸네일 생성 및 저장-thumbnailator라이브러리)

--- src/main/java/com/pofol/main/product/controller/ProdController.java
+++ src/main/java/com/pofol/main/product/controller/ProdController.java
@@ -3,12 +3,25 @@ package com.pofol.main.product.controller;
 import com.pofol.main.product.domain.ProdDto;
 import com.pofol.main.product.service.ProdService;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.nio.Buffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @Slf4j
@@ -27,7 +40,7 @@ public class ProdController {
     }
 
     @GetMapping("/display")
-    public void displayPageGET(Model model) throws Exception{
+    public void displayPageGET(Model model) throws Exception {
         System.out.println("prodService.getList() = " + prodService.getList());
         List<ProdDto> display = prodService.getList();
         model.addAttribute("display", display);
@@ -38,6 +51,71 @@ public class ProdController {
         List<ProdDto> display = prodService.getList();
         model.addAttribute("display", display);
         return "product";
+    }
+
+    @GetMapping("/prodEnroll")
+    public String prodEnrollPageGET() {
+        return "prodEnroll";
+    }
+
+    // 첨부 파일 업로드
+    @PostMapping("/prodEnroll")
+    public void prodEnrollPOST(@RequestParam("uploadFile") MultipartFile[] uploadFile) {
+        // MultipartFile : 뷰에서 전송한 multipart타입 파일을 다룰 수 있도록 해주는 인터페이스
+        System.out.println("uploadFile = " + uploadFile);
+        String uploadFolder = "/Users/hyungjunlim/Documents/programming/echoeats_items";
+
+        // 날짜 폴더 경로
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String formatted = simpleDateFormat.format(date);
+        String datePath = formatted.replace("-", File.separator);
+
+        // 파일 생성
+        File uploadPath = new File(uploadFolder, datePath);
+        if (uploadPath.exists() == false) {
+            uploadPath.mkdirs();
+        }
+
+        for (MultipartFile multipartFile : uploadFile) {
+            // 파일 이름
+            String uploadFileName = multipartFile.getOriginalFilename();
+            // UUID 적용 파일 이름
+            String uuid = UUID.randomUUID().toString();
+            uploadFileName = uuid + "_" + uploadFileName;
+            // 파일 위치, 파일 이름을 합친 File객체
+            File saveFile = new File(uploadPath, uploadFileName);
+            // 파일 저장
+            try {
+                multipartFile.transferTo(saveFile);
+//                // 썸네일 생성
+//                File thumbnailFile = new File(uploadPath, "s_" + uploadFileName);
+//                BufferedImage bo_image = ImageIO.read(saveFile);
+//                // 비율
+//                double ratio = 3;
+//                // 넓이, 높이
+//                int width = (int) (bo_image.getWidth() / ratio);
+//                int height = (int) (bo_image.getHeight() / ratio);
+//                BufferedImage bt_image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+//                Graphics2D graphics = bt_image.createGraphics();
+//                graphics.drawImage(bo_image, 0, 0, width, height, null);
+//                ImageIO.write(bt_image, "jpg", thumbnailFile);
+                /** thumbnailator 라이브러리 사용 **/
+                File thumbnailFile = new File(uploadPath, "s_" + uploadFileName);
+
+                BufferedImage bo_image = ImageIO.read(saveFile);
+                // 비율
+                double ratio = 3;
+                // 넓이, 높이
+                int width = (int) (bo_image.getWidth() / ratio);
+                int height = (int) (bo_image.getHeight() / ratio);
+                Thumbnails.of(saveFile)
+                        .size(width, height)
+                        .toFile(thumbnailFile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/prodEnroll.jsp
@@ -0,0 +1,79 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<html>
+<head>
+    <title>Title</title>
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.js"
+            integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+            crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="form_section">
+    <div class="form_section_title">
+        <label>상품 이미지</label>
+    </div>
+    <div class="form_section_content">
+        <input type="file" multiple id="fileItem" name='uploadFile' style="height: 30px;">
+    </div>
+</div>
+<script>
+    /* 이미지 업로드 */
+    $("input[type='file']").on("change", function(e){
+        alert("동작");
+
+        let formData = new FormData();
+        let fileInput = $('input[name="uploadFile"]');
+        let fileList = fileInput[0].files;
+        let fileObj = fileList[0];
+
+        if (!fileCheck(fileObj.name, fileObj.size)) {
+            return false;
+        }
+
+        for(let i = 0; i < fileList.length; i++){
+            formData.append("uploadFile", fileList[i]);
+            console.log(formData);
+        }
+
+        $.ajax({
+            url : '/prodEnroll',
+            processData : false,
+            contentType : false,
+            data : formData,
+            type : 'POST',
+            dataType : 'json',
+            mimeType: 'multipart/form-data',
+            success : function (result) {
+                alert("success");
+            },
+            error : function () {
+                alert("fail");
+            }
+        });
+
+    });
+
+    /* var, method related with attachFile */
+    let regex = new RegExp("(.*?)\.(jpg|png)$");
+    let maxSize = 10485760; //1MB
+
+    function fileCheck(fileName, fileSize) {
+
+        if (!regex.test(fileName)) {
+            alert("해당 종류의 파일 업로드할 수 없습니다.")
+            return false;
+        }
+
+        if (fileSize >= maxSize) {
+            alert("파일 사이즈 초과");
+            return false;
+        }
+
+        return true;
+
+    }
+</script>
+</body>
+</html>
