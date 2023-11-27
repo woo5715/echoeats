4c7ad81c6f7e779f10cf3a7b493e31ac76c63fe2
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-27 14:48:22 +0900

    [feat]productRepository - productEnroll 구현

---
+++ src/main/java/com/pofol/main/product/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.pofol.main.product.repository;
+
+import com.pofol.main.product.domain.ProductDto;
+
+public interface ProductRepository {
+
+    // 상품 등록
+    public void productEnroll(ProductDto productDto);
+
+}
