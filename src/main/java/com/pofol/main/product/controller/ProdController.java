5b795d7f5008de8ddce3127330dcc3b7f9999126
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-28 17:25:21 +0900

    [chore]시환님과 머지전 필요없는거 삭제

--- src/main/java/com/pofol/main/product/controller/ProdController.java
+++ src/main/java/com/pofol/main/product/controller/ProdController.java
@@ -1,7 +1,5 @@
 package com.pofol.main.product.controller;
 
-import com.pofol.main.product.domain.ProdDto;
-import com.pofol.main.product.service.ProdService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -20,32 +18,6 @@ import java.util.List;
 @Controller
 @Slf4j
 public class ProdController {
-
-    private final ProdService prodService;
-
-    @Autowired
-    public ProdController(ProdService prodService) {
-        this.prodService = prodService;
-    }
-
-    @GetMapping("/main")
-    public String mainPageGET() {
-        return "main";
-    }
-
-    @GetMapping("/display")
-    public void displayPageGET(Model model) throws Exception {
-        System.out.println("prodService.getList() = " + prodService.getList());
-        List<ProdDto> display = prodService.getList();
-        model.addAttribute("display", display);
-    }
-
-    @GetMapping("/product")
-    public String productPageGET(Model model) throws Exception {
-        List<ProdDto> display = prodService.getList();
-        model.addAttribute("display", display);
-        return "product";
-    }
 
     @GetMapping("/prodEnroll")
     public String prodEnrollPageGET() {
