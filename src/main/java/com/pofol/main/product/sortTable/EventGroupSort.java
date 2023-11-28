af16a02790d56b55f7a581dc1b608ada773a6c22
seahwanKim <kimkimseasea@gmail.com>
2023-11-28 17:45:13 +0900

    [feat] 나중에 필요한 클래스 + 인터페이스 작성

---
+++ src/main/java/com/pofol/main/product/sortTable/EventGroupSort.java
@@ -0,0 +1,11 @@
+package com.pofol.main.product.sortTable;
+
+import java.util.List;
+
+public class EventGroupSort implements SortingStrategy{
+
+  @Override
+  public List sort(List items) {
+    return null;
+  }
+}
