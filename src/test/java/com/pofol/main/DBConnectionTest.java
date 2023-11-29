61809320ccc287859b66bc458113501853e251bc
woo5715 <too5715@naver.com>
2023-11-29 10:08:58 +0900

    [chore] 설정 파일 수정

--- src/test/java/com/pofol/main/DBConnectionTest.java
+++ src/test/java/com/pofol/main/DBConnectionTest.java
@@ -6,13 +6,14 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import com.pofol.main.member.repository.MemberRepository;
+import com.pofol.main.member.dto.memberDto;
+import com.pofol.main.member.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.pofol.main.TestDao;
 
 @ExtendWith(SpringExtension.class) // Junit5
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
@@ -22,6 +23,12 @@ public class DBConnectionTest  {
 	
 	@Autowired
 	TestDao dao;
+
+	@Autowired
+	MemberRepository Mdao;
+
+	@Autowired
+	MemberService service;
 	
 	@Test
 	public void test1()throws Exception {
@@ -37,4 +44,19 @@ public class DBConnectionTest  {
 	    System.out.println("dao.now() = "+dao.now());
 	    assertTrue(dao.now()!=null);
 	}
+
+	@Test
+	public void test3(){
+		memberDto exampleMemId = Mdao.select_member("example_mem_id");
+		System.out.println(exampleMemId.toString());
+	}
+
+	@Test
+	public void test4(){
+		memberDto exampleMemId = service.select("example_mem_id");
+		System.out.println(exampleMemId.toString());
+	}
+
+
+
 }
