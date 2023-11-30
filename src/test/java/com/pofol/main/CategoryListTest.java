1ef5334d1438ead4cc0d1909e49a9aa8104d5291
seahwanKim <kimkimseasea@gmail.com>
2023-11-30 17:05:08 +0900

    [test] 중카테고리 리스트 테스트

--- src/test/java/com/pofol/main/CategoryListTest.java
+++ src/test/java/com/pofol/main/CategoryListTest.java
@@ -1,11 +1,17 @@
 package com.pofol.main;
 
+import com.pofol.main.product.category.CategoryDto;
 import com.pofol.main.product.category.CategoryList;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class) // Junit5
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
@@ -15,9 +21,18 @@ public class CategoryListTest {
   CategoryList categoryList;
 
   @Test
-  void CategoryListTest() throws Exception{
+  void MidCategoryList() throws Exception {
 
-      System.out.println("categoryList = " + categoryList.cateList());
+    System.out.println("categoryList = " + categoryList.midCateList("907"));
+    try {
+      List<CategoryDto> midCateList = categoryList.midCateList("907");
+
+      for (CategoryDto categoryDto : midCateList) {
+//        assertThat(categoryDto.getParent_code()).isEqualTo("907");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
 }
