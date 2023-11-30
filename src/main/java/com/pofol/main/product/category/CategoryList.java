58be3988d6a661de95a3bbba018b2f4f5ef1d1b1
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-30 11:28:53 +0900

    [refactor] 카테고리정렬 인터페이스 포매팅

--- src/main/java/com/pofol/main/product/category/CategoryList.java
+++ src/main/java/com/pofol/main/product/category/CategoryList.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface CategoryList {
 
-  // 카테고리 정렬
-  List<CategoryDto> cateList() throws Exception;
+    // 카테고리 정렬
+    List<CategoryDto> cateList() throws Exception;
+
+    // 상품 등록 카테고리
+    List<CategoryDto> enrollCategoryList() throws Exception;
+
 }
