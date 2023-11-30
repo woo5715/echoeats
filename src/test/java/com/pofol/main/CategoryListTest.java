e85228e6c4c5ca60fcffc9fac106c1174652f5b3
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-11-30 11:41:44 +0900

    [refactor] 카테고리 리스트 테스트 포매팅

--- src/test/java/com/pofol/main/CategoryListTest.java
+++ src/test/java/com/pofol/main/CategoryListTest.java
@@ -1,23 +1,33 @@
 package com.pofol.main;
 
 import com.pofol.main.product.category.CategoryList;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@DisplayName("카테고리 리스트 테스트")
 @ExtendWith(SpringExtension.class) // Junit5
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class CategoryListTest {
 
-  @Autowired
-  CategoryList categoryList;
+    @Autowired
+    CategoryList categoryList;
 
-  @Test
-  void CategoryListTest() throws Exception{
+    @DisplayName("카테고리 리스트 테스트")
+    @Test
+    void CategoryList() throws Exception {
 
-      System.out.println("categoryList = " + categoryList.cateList());
+        System.out.println("categoryList = " + categoryList.cateList());
 
-  }
+    }
+
+    @DisplayName("상품 등록할 때, 필요한 카테고리 리스트 테스트")
+    @Test
+    void enrollCategoryList() throws Exception {
+        System.out.println("categoryList = " + categoryList.enrollCategoryList());
+    }
+
 }
