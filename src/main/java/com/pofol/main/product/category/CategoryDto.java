128fd9d8877463b097e7291b8be088bd6c7b6496
seahwanKim <kimkimseasea@gmail.com>
2023-12-01 18:53:20 +0900

    충돌해결1

--- src/main/java/com/pofol/main/product/category/CategoryDto.java
+++ src/main/java/com/pofol/main/product/category/CategoryDto.java
@@ -9,41 +9,41 @@ import java.util.Objects;
 @ToString
 public class CategoryDto {
 
-  private String cat_code;
-  private String tier;
-  private String cat_name;
-  private String parent_code;
+    private String cat_code;
+    private String tier;
+    private String cat_name;
+    private String parent_code;
 
-  public CategoryDto() {
-  }
+    public CategoryDto() {
+    }
 
-  public void setCat_code(String cat_code) {
-    this.cat_code = cat_code;
-  }
+    public void setCat_code(String cat_code) {
+        this.cat_code = cat_code;
+    }
 
-  public void setTier(String tier) {
-    this.tier = tier;
-  }
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
 
-  public void setCat_name(String cat_name) {
-    this.cat_name = cat_name;
-  }
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
 
-  public void setParent_code(String parent_code) {
-    this.parent_code = parent_code;
-  }
+    public void setParent_code(String parent_code) {
+        this.parent_code = parent_code;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    CategoryDto that = (CategoryDto) o;
-    return Objects.equals(cat_code, that.cat_code) && Objects.equals(tier, that.tier) && Objects.equals(cat_name, that.cat_name) && Objects.equals(parent_code, that.parent_code);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoryDto that = (CategoryDto) o;
+        return Objects.equals(cat_code, that.cat_code) && Objects.equals(tier, that.tier) && Objects.equals(cat_name, that.cat_name) && Objects.equals(parent_code, that.parent_code);
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(cat_code, tier, cat_name, parent_code);
-  } 
+    @Override
+    public int hashCode() {
+        return Objects.hash(cat_code, tier, cat_name, parent_code);
+    }
 
 }
