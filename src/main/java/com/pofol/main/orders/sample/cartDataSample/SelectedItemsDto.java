be5d86988aa3ae02fc91f1ff4a7a38815e540e04
hee-no1 <lhw817e@gmail.com>
2023-12-02 14:17:08 +0900

    [refactor] 주문에 필요한 데이터를 수정한다.

--- src/main/java/com/pofol/main/orders/sample/cartDataSample/SelectedItemsDto.java
+++ src/main/java/com/pofol/main/orders/sample/cartDataSample/SelectedItemsDto.java
@@ -1,5 +1,6 @@
 package com.pofol.main.orders.sample.cartDataSample;
 
+import java.util.List;
 import java.util.Objects;
 
 public class SelectedItemsDto {
@@ -7,18 +8,21 @@ public class SelectedItemsDto {
     private Long prod_id;
     private String opt_prod_id;
     private Integer qty;
+    private List<SelectedItemsDto> items;
 
-    public SelectedItemsDto(){}
-
-    public SelectedItemsDto(Long prod_id, Integer qty) {
-        this.prod_id = prod_id;
-        this.qty = qty;
-    }
+    SelectedItemsDto(){}
 
     public SelectedItemsDto(Long prod_id, String opt_prod_id, Integer qty) {
         this.prod_id = prod_id;
         this.opt_prod_id = opt_prod_id;
         this.qty = qty;
+    }
+
+    public SelectedItemsDto(Long prod_id, String opt_prod_id, Integer qty, List<SelectedItemsDto> items) {
+        this.prod_id = prod_id;
+        this.opt_prod_id = opt_prod_id;
+        this.qty = qty;
+        this.items = items;
     }
 
     public Long getProd_id() {
@@ -45,25 +49,24 @@ public class SelectedItemsDto {
         this.qty = qty;
     }
 
+    public List<SelectedItemsDto> getItems() {
+        return items;
+    }
+
+    public void setItems(List<SelectedItemsDto> items) {
+        this.items = items;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SelectedItemsDto that = (SelectedItemsDto) o;
-        return Objects.equals(prod_id, that.prod_id) && Objects.equals(opt_prod_id, that.opt_prod_id) && Objects.equals(qty, that.qty);
+        return Objects.equals(prod_id, that.prod_id) && Objects.equals(opt_prod_id, that.opt_prod_id) && Objects.equals(qty, that.qty) && Objects.equals(items, that.items);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(prod_id, opt_prod_id, qty);
-    }
-
-    @Override
-    public String toString() {
-        return "SelectedItemsDto{" +
-                "prod_id=" + prod_id +
-                ", opt_prod_id='" + opt_prod_id + '\'' +
-                ", qty=" + qty +
-                '}';
+        return Objects.hash(prod_id, opt_prod_id, qty, items);
     }
 }
