59a5daba8617bb77fff11e70ffacaf67b5f79ed6
hee-no1 <lhw817e@gmail.com>
2023-12-02 20:26:18 +0900

    [refactor] 주문서에 필요한 sample 상품Dto를 변경한다.

--- src/main/java/com/pofol/main/orders/sample/productSample/SampleProductRepository.java
+++ src/main/java/com/pofol/main/orders/sample/productSample/SampleProductRepository.java
@@ -2,16 +2,14 @@ package com.pofol.main.orders.sample.productSample;
 
 import com.pofol.main.orders.sample.cartDataSample.SelectedItemsDto;
 
-import java.util.List;
+public interface SampleProductRepository {
 
-public interface RequiredProductRepository {
+    SampleProductDto selectProduct(Long prod_id);
 
-    RequiredProductDto selectProduct(Long prod_id);
+    SampleProductDto selectOptionProduct(String opt_prod_id);
 
-    RequiredProductDto selectOptionProduct(String opt_prod_id);
+    SampleProductDto selectProductInfo(Long prod_id);
 
-    RequiredProductDto selectProductInfo(Long prod_id);
-
-    RequiredProductDto selectRequiredProduct(SelectedItemsDto selectedItemsDto) throws Exception;
+    SampleProductDto selectRequiredProduct(SelectedItemsDto selectedItemsDto) throws Exception;
 
 }
