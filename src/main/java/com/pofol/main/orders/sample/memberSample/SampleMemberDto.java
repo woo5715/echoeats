335261cc6d5799fa33f93d3ba904746d5aee668f
hee-no1 <lhw817e@gmail.com>
2023-12-02 20:24:24 +0900

    [fea] 주문서에 필요한 sample 회원Dto를 구현한다.

--- src/main/java/com/pofol/main/orders/sample/memberSample/SampleMemberDto.java
+++ src/main/java/com/pofol/main/orders/sample/memberSample/SampleMemberDto.java
@@ -1,8 +1,11 @@
 package com.pofol.main.orders.sample.memberSample;
 
+import lombok.Getter;
+
 import java.util.Objects;
 
-public class MemberDtoSample {
+@Getter
+public class SampleMemberDto {
     private String mem_id;
     private Long own_cp_id;
     private String gd_name;
@@ -10,8 +13,8 @@ public class MemberDtoSample {
     private String mem_email;
     private String mem_phone;
 
-    public MemberDtoSample(){}
-    public MemberDtoSample(String mem_id, Long own_cp_id, String gd_name, String mem_name, String mem_email, String mem_phone) {
+    public SampleMemberDto(){}
+    public SampleMemberDto(String mem_id, Long own_cp_id, String gd_name, String mem_name, String mem_email, String mem_phone) {
         this.mem_id = mem_id;
         this.own_cp_id = own_cp_id;
         this.gd_name = gd_name;
@@ -20,11 +23,35 @@ public class MemberDtoSample {
         this.mem_phone = mem_phone;
     }
 
+    public void setMem_id(String mem_id) {
+        this.mem_id = mem_id;
+    }
+
+    public void setOwn_cp_id(Long own_cp_id) {
+        this.own_cp_id = own_cp_id;
+    }
+
+    public void setGd_name(String gd_name) {
+        this.gd_name = gd_name;
+    }
+
+    public void setMem_name(String mem_name) {
+        this.mem_name = mem_name;
+    }
+
+    public void setMem_email(String mem_email) {
+        this.mem_email = mem_email;
+    }
+
+    public void setMem_phone(String mem_phone) {
+        this.mem_phone = mem_phone;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        MemberDtoSample that = (MemberDtoSample) o;
+        SampleMemberDto that = (SampleMemberDto) o;
         return Objects.equals(mem_id, that.mem_id) && Objects.equals(own_cp_id, that.own_cp_id) && Objects.equals(gd_name, that.gd_name) && Objects.equals(mem_name, that.mem_name) && Objects.equals(mem_email, that.mem_email) && Objects.equals(mem_phone, that.mem_phone);
     }
 
