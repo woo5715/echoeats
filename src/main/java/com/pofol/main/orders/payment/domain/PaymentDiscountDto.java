77995c9a9d309387738a7c67576a24ea7f16aa29
hee-no1 <lhw817e@gmail.com>
2023-12-05 11:22:56 +0900

    pull테스트

--- src/main/java/com/pofol/main/orders/payment/domain/PaymentDiscountDto.java
+++ src/main/java/com/pofol/main/orders/payment/domain/PaymentDiscountDto.java
@@ -9,7 +9,7 @@ public class PaymentDiscountDto {
 
     private Long ord_id; //주문번호
     private Integer coupon_disc;  //쿠폰할인금액
-    private Integer reserves_used; //적립금사용금액
+    private Integer point_used; //적립금사용금액
 
     //ajax 용도
     private Integer tot_prod_price; //총 주문금액
@@ -26,8 +26,8 @@ public class PaymentDiscountDto {
         this.coupon_disc = coupon_disc;
     }
 
-    public void setReserves_used(Integer reserves_used) {
-        this.reserves_used = reserves_used;
+    public void setPoint_used(Integer point_used) {
+        this.point_used = point_used;
     }
 
     public void setTot_prod_price(Integer tot_prod_price) {
@@ -47,12 +47,12 @@ public class PaymentDiscountDto {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PaymentDiscountDto that = (PaymentDiscountDto) o;
-        return Objects.equals(ord_id, that.ord_id) && Objects.equals(coupon_disc, that.coupon_disc) && Objects.equals(reserves_used, that.reserves_used) && Objects.equals(tot_prod_price, that.tot_prod_price) && Objects.equals(tot_pay_price, that.tot_pay_price) && Objects.equals(dlvy_fee, that.dlvy_fee);
+        return Objects.equals(ord_id, that.ord_id) && Objects.equals(coupon_disc, that.coupon_disc) && Objects.equals(point_used, that.point_used) && Objects.equals(tot_prod_price, that.tot_prod_price) && Objects.equals(tot_pay_price, that.tot_pay_price) && Objects.equals(dlvy_fee, that.dlvy_fee);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ord_id, coupon_disc, reserves_used, tot_prod_price, tot_pay_price, dlvy_fee);
+        return Objects.hash(ord_id, coupon_disc, point_used, tot_prod_price, tot_pay_price, dlvy_fee);
     }
 
     @Override
@@ -60,7 +60,7 @@ public class PaymentDiscountDto {
         return "PaymentDiscountDto{" +
                 "ord_id=" + ord_id +
                 ", coupon_disc=" + coupon_disc +
-                ", reserves_used=" + reserves_used +
+                ", reserves_used=" + point_used +
                 ", tot_prod_price=" + tot_prod_price +
                 ", tot_pay_price=" + tot_pay_price +
                 ", dlvy_fee=" + dlvy_fee +
--- src/main/webapp/resources/order/js/checkoutJS.js
+++ src/main/webapp/resources/order/js/checkoutJS.js
@@ -1,22 +1,35 @@
-//적립금
-function updateValue(input){
+// document.addEventListener('DOMContentLoaded', function () {
+//     addComma();
+// });
 
-    if(input === 0 && input === null){ //처음 입력할 때
-        input.value = input.value.replace(/[^1-9]/g, '');
-    } else{
-        input.value = input.value.replace(/[^0-9]/g, '');
-    }
+// //금액에 콤마 추가
+// let addComma = function(){
+//     $(".money").each(function () {
+//         let currentValue = $(this).text();
+//         let formattedValue = Number(currentValue).toLocaleString();
+//         $(this).text(formattedValue);
+//     });
+// }
+//
+// //금액에 콤마 제거
+// let removeComma = function(){
+//     $(".money").each(function () {
+//         let currentValue = $(this).text();
+//         let formattedValue = currentValue.replace(/,/g, '');
+//         $(this).text(formattedValue);
+//     });
+// }
 
-
+//쿠폰, 적립금 ajax로 보내기
+let ajaxData = function(){
 
     let tot_prod_price = document.getElementById('tot_prod_price').innerHTML;
-    let reserves_used = document.getElementById('inputReservesUsed').value;
+    let point_used = document.getElementById('inputPointUsed').value.replace(/[^0-9]/g, '');
     let dlvy_fee = document.getElementById('dlvy_fee').innerHTML;
-
 
     let paymentDiscount = {
         tot_prod_price: tot_prod_price,
-        reserves_used: reserves_used,
+        point_used: point_used,
         dlvy_fee: dlvy_fee
     }
 
@@ -31,26 +44,60 @@ function updateValue(input){
             data: JSON.stringify(paymentDiscount),
             success: function(result){
                 paymentData = result;
-                if(paymentData.reserves_used === null) {
-                    $("#outputReservesUsed").html(0);
+                if(paymentData.point_used === null) {
+                    $("#sign").html("");
+                    $("#outputPointUsed").html(0);
                 }else{
-                    $("#outputReservesUsed").html("<span class='css-rfpchb eahaaoi3'>-</span>"+paymentData.reserves_used);
+                    $("#sign").html("-");
+                    $("#outputPointUsed").html(paymentData.point_used);
                 }
                 //나중에 여기에 쿠폰도 추가해야한다.
                 if(paymentData.tot_pay_price === null){
                     $("#tot_pay_price").html(0);
                 }else{
                     $("#tot_pay_price").html(paymentData.tot_pay_price);
+                    $("#payment").html(paymentData.tot_pay_price);
                 }
-
-
             },
             error: function(){ alert("error!!" ); }
         })
-
-
     });
-
 }
+
+//적립금 입력
+function updateValue(input){
+    console.log("Before: " + input.value);
+    let lastChar = input.value[input.value.length-1]; //마지막 글자
+    console.log("마지막 글자: " + lastChar);
+    let sliceResult = input.value.slice(0, -1); //첫글자부터 마지막 전 글자까지
+    console.log("첫 글자부터 마지막 전 글자까지: " + sliceResult);
+
+    if (input.value === '' || input.value === '0') {
+        input.value = input.value.replace(/[^1-9]/g, '');
+    }else if(isNaN(lastChar)){ //lastChar이 숫자가 아닐 때
+        let sanitizedValue = lastChar.replace(/[^0-9]/g, '');
+
+        input.value = sliceResult + sanitizedValue;
+    }
+
+    let point = document.getElementById('point').innerHTML;
+
+    if(input.value*1 > point*1){
+        input.value = point;
+    }
+
+    console.log("After: " + input.value);
+
+    ajaxData();
+}
+
+
+//적립금 모두 사용 버튼
+$("#allUseBtn").click(function(){
+    let point = document.getElementById('point').innerHTML;
+    document.getElementById('inputPointUsed').value = point;
+
+    ajaxData();
+})
 
 
