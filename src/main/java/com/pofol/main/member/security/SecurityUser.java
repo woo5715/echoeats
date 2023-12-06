6f7a4e26e2c35d18bcfb567b3b60fcd86358df92
woo5715 <too5715@naver.com>
2023-12-06 15:03:59 +0900

    [feat]  기능 추가 및 수정

--- src/main/java/com/pofol/main/member/security/SecurityUser.java
+++ src/main/java/com/pofol/main/member/security/SecurityUser.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-@Builder
+
 public class SecurityUser implements UserDetails {
 
     private Collection<? extends GrantedAuthority> authorities;
