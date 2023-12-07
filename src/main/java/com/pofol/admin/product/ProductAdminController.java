e02904b9deec1d81a50ba8475c738a31db2e94db
seahwanKim <kimkimseasea@gmail.com>
2023-12-07 16:29:18 +0900

    [feat] 상품관리 검색 기능을 구현한다

--- src/main/java/com/pofol/admin/product/ProductAdminController.java
+++ src/main/java/com/pofol/admin/product/ProductAdminController.java
@@ -1,10 +1,14 @@
 package com.pofol.admin.product;
 
+import com.pofol.main.product.domain.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -14,10 +18,29 @@ public class ProductAdminController {
     private final ProductAdminService productAdminService;
 
     @GetMapping("/product/list")
-    public String getAdminProductList() {
+    public String getAdminProductList(SearchProductAdminCondition searchProductAdminCondition, Model model, String selling, String display) {
 
+        try {
+            // 상품 관리자페이지에 상품조회
+//            List<ProductDto> productAdminList = productAdminService.getProductAdminList();
+//            model.addAttribute("product", productAdminList);
 
+            // 페이징
+            Integer totalCount = productAdminService.getProductAdminSearchCount(searchProductAdminCondition);
+            PageHandler pageHandler = new PageHandler(totalCount, searchProductAdminCondition);
+            model.addAttribute("ph", pageHandler);
 
+            System.out.println("selling = " + selling);
+            System.out.println("display = " + display);
+            System.out.println("keyword = " + searchProductAdminCondition.getKeyword());
+
+            // 상품 관리자 페이지에서 조건에 따라 상품조회
+            List<ProductDto> productAdminList = productAdminService.getProductAdminSearchList(searchProductAdminCondition, selling, display);
+            model.addAttribute("product", productAdminList);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return "admin/product/productAdminList";
     }
