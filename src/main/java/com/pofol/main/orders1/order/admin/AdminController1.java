f81d7cc22b9df71c33edea8d48ce95f5e9f848ad
hee-no1 <lhw817e@gmail.com>
2023-12-08 16:04:15 +0900

    [refactor] order폴더와 order1폴더안에 빈 이름 충돌, 클래스명 변경

--- src/main/java/com/pofol/main/orders1/order/admin/AdminController1.java
+++ src/main/java/com/pofol/main/orders1/order/admin/AdminController1.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.pofol.main.orders1.order.domain.CodeTableDto;
 import com.pofol.main.orders1.order.domain.PageHandler;
 import com.pofol.main.orders1.order.domain.SearchOrderCondition;
-import com.pofol.main.orders1.order.repository.OrderRepository;
+import com.pofol.main.orders1.order.repository.OrderRepository1;
 
 @Controller
 @RequestMapping("/admin1")
@@ -26,7 +26,7 @@ public class AdminController1 {
 	@Autowired
 	AdminOrderRepository adminOrdRpo;
 	@Autowired
-	OrderRepository ordRop;
+    OrderRepository1 ordRop;
 	
 	@GetMapping("/order/list")
     public String orderList(SearchOrderCondition sc, Model m, HttpServletRequest request){
