1437295d71eef754fc910878facd14bd46b165b3
woo5715 <too5715@naver.com>
2023-12-13 16:45:16 +0900

    [test]   테스트

--- src/main/java/com/pofol/main/member/controller/GradeController.java
+++ src/main/java/com/pofol/main/member/controller/GradeController.java
@@ -1,6 +1,8 @@
 package com.pofol.main.member.controller;
 
 import com.pofol.main.member.dto.GradeDto;
+import com.pofol.main.member.dto.abcDto;
+import com.pofol.main.member.dto.abcdDto;
 import com.pofol.main.member.service.GradeService;
 import org.apache.tomcat.util.descriptor.web.ContextHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -36,5 +40,20 @@ public class GradeController {
         System.out.println(user123);
         return user123;
     }
+
+    @GetMapping("/abc")
+    public String abc(){
+        return "member/abc";
+    }
+
+    @PostMapping("/abcd")
+    @ResponseBody
+    public String abcd(@RequestBody abcdDto abcdDto){
+        System.out.println(abcdDto);
+        System.out.println(abcdDto.getAbcDto().toString());
+        System.out.println(abcdDto.getD());
+        return "member/abc";
+    }
+
 
 }
