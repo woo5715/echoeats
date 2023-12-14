e5a716f72d8687884e17346a2c47d77f485b1bac
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-12-15 07:01:44 +0900

    [feat] insertImage in ProductRepository 구현

--- src/main/java/com/pofol/main/product/repository/ProductRepository.java
+++ src/main/java/com/pofol/main/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.pofol.main.product.repository;
 
 import com.pofol.main.product.domain.ProductDto;
+import com.pofol.main.product.domain.ProductImageDto;
 
 public interface ProductRepository {
 
@@ -9,5 +10,8 @@ public interface ProductRepository {
 
     // product테이블 행 개수
     int count() throws Exception;
+
+    // 상품 이미지 등록
+    void insertImage(ProductImageDto productImageDto) throws Exception;
 
 }
