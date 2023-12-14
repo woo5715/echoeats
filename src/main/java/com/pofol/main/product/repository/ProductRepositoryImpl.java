656cd49bfe4b6524f39d3778c42aa8401771ec0f
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-12-15 07:04:25 +0900

    [feat] insertImage in ProductRepositoryImpl 구현

--- src/main/java/com/pofol/main/product/repository/ProductRepositoryImpl.java
+++ src/main/java/com/pofol/main/product/repository/ProductRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pofol.main.product.repository;
 
 import com.pofol.main.product.domain.ProductDto;
+import com.pofol.main.product.domain.ProductImageDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,22 +9,28 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
-  private final String namespace = "ecoeats.productMapper.";
-  private final SqlSession session;
+    private final String namespace = "ecoeats.productMapper.";
+    private final SqlSession session;
 
-  @Autowired
-  public ProductRepositoryImpl(SqlSession session) {
-    this.session = session;
-  }
+    @Autowired
+    public ProductRepositoryImpl(SqlSession session) {
+        this.session = session;
+    }
 
-  // 상품 등록(INSERT)
-  @Override
-  public void insert(ProductDto productDto) throws Exception {
-    session.insert(namespace + "insert", productDto);
-  }
+    // 상품 등록(INSERT)
+    @Override
+    public void insert(ProductDto productDto) throws Exception {
+        session.insert(namespace + "insert", productDto);
+    }
 
-  @Override
-  public int count() throws Exception {
-    return session.selectOne(namespace + "count");
-  }
+    @Override
+    public int count() throws Exception {
+        return session.selectOne(namespace + "count");
+    }
+
+    // 상품 이미지 등록(INSERT)
+    @Override
+    public void insertImage(ProductImageDto productImageDto) throws Exception {
+        session.insert(namespace + "insertImage", productImageDto);
+    }
 }
