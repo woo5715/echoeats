b943449561b8f4fca4e0933cce8ba7f2a3490690
woo5715 <too5715@naver.com>
2023-12-14 13:16:13 +0900

    [refactor]  회원 등급 갱신 코드

--- src/main/java/com/pofol/main/member/task/GradeTask.java
+++ src/main/java/com/pofol/main/member/task/GradeTask.java
@@ -4,10 +4,14 @@ import com.pofol.main.member.dto.GradeDto;
 import com.pofol.main.member.dto.MemberDto;
 import com.pofol.main.member.service.GradeService;
 import com.pofol.main.member.service.MemberService;
+import com.pofol.main.orders.payment.domain.PaymentDto;
+import com.pofol.main.orders.payment.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -19,9 +23,12 @@ public class GradeTask {
     @Autowired
     GradeService gradeService;
 
+    @Autowired
+    PaymentRepository paymentRepository;
 
-    @Scheduled(cron = "0 40 14 * * *")
-    //@Scheduled(cron = "*/10 * * * * *")
+
+    //@Scheduled(cron = "0 40 14 * * *")
+    @Scheduled(cron = "*/10 * * * * *")
     public void testMethod() throws Exception {
         System.out.println("배치 실행중~~~~~~~~~~~~~~~~");
 
@@ -29,47 +36,62 @@ public class GradeTask {
         //이 날짜를 이용하여 member의 update_date와 일치한 회원들을 가져온다
         //반복문 안에 회원들을 하나씩꺼내서 전월 사용한 금액을 조회한다
         //등급 계산 메서드에 (결제금액, 등급Dto)를 넣는다
-        //리턴받은 등급을 그회원의 등급에 update한다
+        //리턴받은 등급을 그 회원의 등급에 update한다
 
-        int money = 250000;
-        int[] grade = new int[5];
-        String[] grade_name = new String[5];
-        String member_grade = "";
-        int i = 4;
+
+
 
 
         List<GradeDto> gradeDtos = gradeService.show_list();
-        for (GradeDto gradeDto : gradeDtos) {
-            System.out.println(gradeDto);
-            grade[i] = gradeDto.getGd_condition();
-            grade_name[i]=gradeDto.getGd_name();
-            i--;
-        }
-
-        for(int j=4 ; j>=0 ; j--){
-            System.out.println(grade_name[j]+" : "+grade[j]);
-        }
-        //메서드로 만들 것 (전월 결제 금액, gradeDtos)
-        if(0<= money && money <=grade[0]){
-            member_grade = grade_name[0];
-        } else if (grade[0]< money && money<=grade[1]) {
-            member_grade = grade_name[1];
-        } else if (grade[1]< money && money<=grade[2]) {
-            member_grade = grade_name[2];
-        } else if (grade[2]< money && money<=grade[3]){
-            member_grade = grade_name[3];
-        } else if (grade[3]< money && money<=grade[4] || grade[4]< money){
-            member_grade = grade_name[4];
-        }
 
 
-        List<MemberDto> memberDtos = memberService.check_grade("2023-12-04");
-//        for (MemberDto memberDto : memberDtos) {
-//            System.out.println(memberDto);
-//        }
+        LocalDate currentDate = LocalDate.now();
+
+        LocalDate dayPlusOne = currentDate.plusDays(1);
+
+        // 전 달 날짜 계산
+        LocalDate MonthMinusOne = currentDate.minusMonths(1);
+
+
+        String currentDateStr = String.valueOf(currentDate);
+        String dayPlusOneStr = String.valueOf(dayPlusOne);
+        String MonthMinusOneStr = String.valueOf(MonthMinusOne);
+
+
+
+        List<MemberDto> memberDtos = memberService.check_grade(currentDateStr);
+        for (MemberDto memberDto : memberDtos) {
+            System.out.println(memberDto);
+            String memId = memberDto.getMem_id();
+            PaymentDto paymentDto = new PaymentDto(memId, MonthMinusOneStr,dayPlusOneStr);//nullpointer나면 어떻게 하냐?
+            Integer i1 = paymentRepository.selectTotalPrice(paymentDto);
+            System.out.println("i1 : "+i1);
+            String s = cal_grade(i1, gradeDtos);
+            System.out.println(s);
+            memberDto.setGd_name(s);
+            int update = memberService.update_grade(memberDto);
+        }
+
 
 
     }
+
+    public String cal_grade(int money,List<GradeDto> gradeDtos ){
+        String member_grade = "";
+
+        for (GradeDto gradeDto : gradeDtos) {
+            if(money>= gradeDto.getGd_condition()){
+                member_grade = gradeDto.getGd_name();
+                break;
+            }
+        }
+
+
+        return member_grade;
+    }
+
+
+
 }
 
 
