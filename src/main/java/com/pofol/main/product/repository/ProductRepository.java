ff9d60fa10334d52c3500fa2ba5fd2c2ca7b629e
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-12-15 10:33:08 +0900

    [feat]상품 옵션 등록 추가

--- src/main/java/com/pofol/main/product/repository/ProductRepository.java
+++ src/main/java/com/pofol/main/product/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.pofol.main.product.repository;
 
+import com.pofol.main.product.domain.OptionProductDto;
 import com.pofol.main.product.domain.ProductDto;
 import com.pofol.main.product.domain.ProductImageDto;
 
@@ -13,5 +14,8 @@ public interface ProductRepository {
 
     // 상품 이미지 등록
     void insertImage(ProductImageDto productImageDto) throws Exception;
+
+    // 상품 옵션 등록
+    void insertOptionProduct(OptionProductDto optionProductDto) throws Exception;
 
 }
