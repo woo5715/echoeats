f844f4f2c9d33940247793c67b777ccbeb3d4549
woo5715 <too5715@naver.com>
2023-12-18 10:22:15 +0900

    [feat] 쿠폰 다운로드를 위한 기능 추가

--- src/main/java/com/pofol/main/member/service/CouponService.java
+++ src/main/java/com/pofol/main/member/service/CouponService.java
@@ -1,10 +1,19 @@
 package com.pofol.main.member.service;
 
+import com.pofol.main.member.dto.CouponDownloadDto;
 import com.pofol.main.member.dto.CouponDto;
 import com.pofol.main.member.dto.CouponJoinDto;
+import com.pofol.main.member.dto.MemCouponDto;
 
 import java.util.List;
 
 public interface CouponService {
-    List<CouponJoinDto> getCoupon(String id);
+    List<CouponJoinDto> getCouponJoin(String id);
+    List<CouponDownloadDto> showDownloadList();
+
+    int update_cp_qty(CouponJoinDto couponJoinDto);
+    int insert_memCoupon(MemCouponDto memCouponDto);
+
+    int update_minus_cp_qty(int dw_id);
+    List<Integer> select_downloaded_dw_id(String mem_id);
 }
--- src/main/java/com/pofol/main/member/service/CouponServiceImpl.java
+++ src/main/java/com/pofol/main/member/service/CouponServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pofol.main.member.service;
 
+import com.pofol.main.member.dto.CouponDownloadDto;
 import com.pofol.main.member.dto.CouponDto;
 import com.pofol.main.member.dto.CouponJoinDto;
+import com.pofol.main.member.dto.MemCouponDto;
 import com.pofol.main.member.repository.CouponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,32 @@ public class CouponServiceImpl implements CouponService{
     @Autowired
     CouponRepository couponRepository;
     @Override
-    public List<CouponJoinDto> getCoupon(String id) {
+    public List<CouponJoinDto> getCouponJoin(String id) {
         return couponRepository.selectMembersWithCoupons(id);
+    }
+
+    @Override
+    public List<CouponDownloadDto> showDownloadList() {
+        return couponRepository.select_downloadList();
+    }
+
+    @Override
+    public int update_cp_qty(CouponJoinDto couponJoinDto) {
+        return couponRepository.update_cp_qty(couponJoinDto);
+    }
+
+    @Override
+    public int insert_memCoupon(MemCouponDto memCouponDto) {
+        return couponRepository.insert_memCoupon(memCouponDto);
+    }
+
+    @Override
+    public int update_minus_cp_qty(int dw_id) {
+        return couponRepository.update_minus_cp_qty(dw_id);
+    }
+
+    @Override
+    public List<Integer> select_downloaded_dw_id(String mem_id) {
+        return couponRepository.select_downloaded_dw_id(mem_id);
     }
 }
