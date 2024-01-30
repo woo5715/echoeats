f123246a86022cddd251814fc365523160a85604
woo5715 <too5715@naver.com>
2024-01-30 13:00:23 +0900

    [feat] 예외 로그 추가

--- src/main/java/com/pofol/main/member/service/MemberServiceImpl.java
+++ src/main/java/com/pofol/main/member/service/MemberServiceImpl.java
@@ -2,6 +2,9 @@ package com.pofol.main.member.service;
 
 import com.pofol.main.member.dto.MemberDto;
 import com.pofol.main.member.repository.MemberRepository;
+import com.pofol.main.member.task.GradeTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,17 @@ public class MemberServiceImpl implements MemberService {
     @Autowired
     MemberRepository memberRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
+
 
     @Override
-    public MemberDto select(String id) throws Exception {
-        return memberRepository.selectMember(id);
+    public MemberDto select(String id)  {
+        try {
+            return memberRepository.selectMember(id);
+        }catch (Exception e){
+            logger.error("회원 조회 실패 {}", e.getMessage());
+            return null;
+        }
     }
 
 
@@ -27,13 +37,23 @@ public class MemberServiceImpl implements MemberService {
      }
 
     @Override
-    public List<MemberDto> check_grade(String date)throws Exception {
-        return memberRepository.checkGrade(date);
+    public List<MemberDto> check_grade(String date) {
+        try {
+            return memberRepository.checkGrade(date);
+        }catch (Exception e){
+            logger.error("회원 등급 체크 실패 {}", e.getMessage());
+            return null;
+        }
     }
 
     @Override
-    public int update_grade(MemberDto memberDto) throws Exception {
-        return memberRepository.updateGrade(memberDto);
+    public int update_grade(MemberDto memberDto)  {
+        try {
+            return memberRepository.updateGrade(memberDto);
+        }catch (Exception e){
+            logger.error("회원 등급 업데이트 실패 {}", e.getMessage());
+            return -1;
+        }
     }
 
 }
