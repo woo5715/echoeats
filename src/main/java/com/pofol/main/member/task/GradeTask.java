6fe35975d2111b022cec98deafb1630907b989e1
woo5715 <too5715@naver.com>
2024-01-30 11:19:35 +0900

    [feat] 코드 수정 및 조건문 추가

--- src/main/java/com/pofol/main/member/task/GradeTask.java
+++ src/main/java/com/pofol/main/member/task/GradeTask.java
@@ -2,10 +2,14 @@ package com.pofol.main.member.task;
 
 import com.pofol.main.member.dto.GradeDto;
 import com.pofol.main.member.dto.MemberDto;
+import com.pofol.main.member.service.CouponServiceImpl;
 import com.pofol.main.member.service.GradeService;
 import com.pofol.main.member.service.MemberService;
 import com.pofol.main.orders.payment.domain.PaymentDto;
 import com.pofol.main.orders.payment.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,64 +19,70 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class GradeTask {
 
-    @Autowired
-    MemberService memberService;
+    private static final Logger logger = LoggerFactory.getLogger(GradeTask.class);
 
-    @Autowired
-    GradeService gradeService;
 
-    @Autowired
-    PaymentRepository paymentRepository;
+   private final MemberService memberService;
+
+    private final GradeService gradeService;
+
+    private final PaymentRepository paymentRepository;
 
 
     @Scheduled(cron = "0 0 0 * * *")
-    //@Scheduled(cron = "*/10 * * * * *")
-    public void testMethod() throws Exception {
-        System.out.println("================등급 갱신================");
+//    @Scheduled(cron = "*/10 * * * * *")
+    public void testMethod()  {
+        logger.info("================등급 갱신================");
 
 
 
         //db에서 등급을 가져온다
         List<GradeDto> gradeDtos = gradeService.show_list();
 
-        //오늘 날짜를 가져온다(등급갱신일을 조회하는 데 사용)
+        //오늘 날짜를 가져온다
         LocalDate currentDate = LocalDate.now();
 
         //밑에 두 날짜는 한 달동안 총결제한 금액을 가져오는데 사용
         //오늘 날짜에서 하루를 더함
-        LocalDate dayPlusOne = currentDate.plusDays(1);
+        String dayPlusOneStr = String.valueOf(currentDate.plusDays(1));
         //전 달 날짜
-        LocalDate MonthMinusOne = currentDate.minusMonths(1);
-
-        //문자열로 조회하기 때문에 모두 문자열로 형변환
-        String currentDateStr = String.valueOf(currentDate);
-        String dayPlusOneStr = String.valueOf(dayPlusOne);
-        String MonthMinusOneStr = String.valueOf(MonthMinusOne);
+        String MonthMinusOneStr = String.valueOf(currentDate.minusMonths(1));
 
 
-        //등급갱신일이 오늘 날짜인 회원들을 조회
-        List<MemberDto> memberDtos = memberService.check_grade(currentDateStr);
-        for (MemberDto memberDto : memberDtos) {
-            System.out.println(memberDto);
-            String memId = memberDto.getMem_id();
-            //회원id, 전 달 날짜, 하루를 더한 날짜를 이용해서 한 달동안 결제한 금액을 가져온다
-            PaymentDto paymentDto = new PaymentDto(memId, MonthMinusOneStr,dayPlusOneStr);
-            Integer i1 = paymentRepository.selectTotalPrice(paymentDto);
-            System.out.println("i1 : "+i1);
-            //결제한 금액, 등급dto를 등급을 계산
-            String s = cal_grade(i1, gradeDtos);
-            System.out.println(s);
-            //반환된 등급을 memberDto에 넣고
-            memberDto.setGd_name(s);
-            //회원 등급 수정
-            int update = memberService.update_grade(memberDto);
+        //등급갱신일(회원이 가입한 날짜의 월,일)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd");
+        String format_currentDate = formatter.format(currentDate);
+
+        try {
+            //등급갱신일이 오늘 날짜인 회원들을 조회
+            List<MemberDto> memberDtos = memberService.check_grade(format_currentDate);
+            for (MemberDto memberDto : memberDtos) {
+                //회원id, 전 달 날짜, 하루를 더한 날짜를 이용해서 한 달동안 결제한 금액을 가져온다
+                PaymentDto paymentDto = new PaymentDto(memberDto.getMem_id(), MonthMinusOneStr, dayPlusOneStr);
+                Integer monthPay = paymentRepository.selectTotalPrice(paymentDto);
+
+                if (monthPay != null) {
+                    //결제한 금액, 등급dto로 등급을 계산하여 반환된 등급을 memberDto에 넣는다
+                    String s = cal_grade(monthPay, gradeDtos);
+                    logger.info(memberDto.getMem_id()+" : "+monthPay+" || "+ memberDto.getGd_name() +" -> "+s);
+                    memberDto.setGd_name(s);
+                    //회원 등급 수정
+                    memberService.update_grade(memberDto);
+                }
+            }
+        }catch (Exception e){
+            logger.error(e.toString());
         }
 
 
 
     }
+
+
+
 
     public String cal_grade(int money,List<GradeDto> gradeDtos ){
         String member_grade = "";
@@ -91,3 +101,5 @@ public class GradeTask {
 
 
 }
+
+//i1이 null이면??
