4d68be7f5f27098490c886d57649d16082e6784f
woo5715 <too5715@naver.com>
2024-01-30 11:23:17 +0900

    [refactor] 롬복 getter 추가 및 필요없는 setter삭제

--- src/main/java/com/pofol/main/member/dto/MemberDto.java
+++ src/main/java/com/pofol/main/member/dto/MemberDto.java
@@ -1,7 +1,10 @@
 package com.pofol.main.member.dto;
 
-import java.util.Date;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 public class MemberDto {
 
     private String mem_id;
@@ -115,145 +118,49 @@ public class MemberDto {
                 '}';
     }
 
-    public String getMem_id() {
-        return mem_id;
-    }
 
     public void setMem_id(String mem_id) {
         this.mem_id = mem_id;
     }
 
-    public String getGd_name() {
-        return gd_name;
-    }
 
     public void setGd_name(String gd_name) {
         this.gd_name = gd_name;
     }
 
-    public String getMem_code_id() {
-        return mem_code_id;
-    }
-
-    public void setMem_code_id(String mem_code_id) {
-        this.mem_code_id = mem_code_id;
-    }
-
-    public String getMem_pwd() {
-        return mem_pwd;
-    }
 
     public void setMem_pwd(String mem_pwd) {
         this.mem_pwd = mem_pwd;
     }
 
-    public String getMem_name() {
-        return mem_name;
-    }
 
     public void setMem_name(String mem_name) {
         this.mem_name = mem_name;
-    }
-
-    public String getMem_email() {
-        return mem_email;
-    }
-
-    public void setMem_email(String mem_email) {
-        this.mem_email = mem_email;
-    }
-
-    public String getMem_phone() {
-        return mem_phone;
-    }
-
-    public void setMem_phone(String mem_phone) {
-        this.mem_phone = mem_phone;
-    }
-
-    public String getMem_gender() {
-        return mem_gender;
-    }
-
-    public void setMem_gender(String mem_gender) {
-        this.mem_gender = mem_gender;
-    }
-
-    public String getMem_birth() {
-        return mem_birth;
-    }
-
-    public void setMem_birth(String mem_birth) {
-        this.mem_birth = mem_birth;
-    }
-
-
-    public int getPoint() {
-        return point;
     }
 
     public void setPoint(int point) {
         this.point = point;
     }
 
-    public String getPer_info_col() {
-        return per_info_col;
-    }
-
-    public void setPer_info_col(String per_info_col) {
-        this.per_info_col = per_info_col;
-    }
-
-    public String getFree_ship_recep() {
-        return free_ship_recep;
-    }
-
-    public void setFree_ship_recep(String free_ship_recep) {
-        this.free_ship_recep = free_ship_recep;
-    }
-
-    public String getRg_date() {
-        return rg_date;
-    }
-
     public void setRg_date(String rg_date) {
         this.rg_date = rg_date;
     }
 
-    public String getRg_num() {
-        return rg_num;
-    }
 
     public void setRg_num(String rg_num) {
         this.rg_num = rg_num;
-    }
-
-    public String getMd_date() {
-        return md_date;
     }
 
     public void setMd_date(String md_date) {
         this.md_date = md_date;
     }
 
-    public String getMd_num() {
-        return md_num;
-    }
-
     public void setMd_num(String md_num) {
         this.md_num = md_num;
     }
 
-    public String getUpdate_date() {
-        return update_date;
-    }
-
     public void setUpdate_date(String update_date) {
         this.update_date = update_date;
-    }
-
-    public String getRole() {
-        return role;
     }
 
     public void setRole(String role) {
