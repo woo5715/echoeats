25de7bb9ebbef1b866ecc9452c9057acd84c727c
woo5715 <too5715@naver.com>
2024-01-30 11:40:38 +0900

    [feat] 트랜잭션 매니저 추가

--- src/main/java/com/pofol/main/member/service/MemberService.java
+++ src/main/java/com/pofol/main/member/service/MemberService.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public interface MemberService {
 
-    MemberDto select(String id) throws Exception;
+    MemberDto select(String id) ;
 
     int signin(MemberDto memberDto) throws Exception;
 
-    List<MemberDto> check_grade(String date)throws Exception;
+    List<MemberDto> check_grade(String date);
 
-    int update_grade(MemberDto memberDto) throws Exception;
+    int update_grade(MemberDto memberDto) ;
 
 }
